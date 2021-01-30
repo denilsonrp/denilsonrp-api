b84af12bcf76e2117838b9aa7ba31ab226eea20c
Denilson <denilsonraimundo17@gmail.com>
2021-01-30 08:48:33 -0300

    :tada: Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e085e08ceb08c38e5fae9297eb04487a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '26755202-e0a1-4a94-ac46-015fb0aa082f'
+};
